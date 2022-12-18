7cdc03bc6f89af8f3a24b368967af85a1aa2c632
jhun1020 <jhun1020.kh@gmail.com>
2022-12-19 00:47:25 +0900

    survey

--- src/main/java/com/jhunlab/study_suvlets/PollistServlet.java
+++ src/main/java/com/jhunlab/study_suvlets/PollistServlet.java
@@ -40,6 +40,7 @@ import jakarta.servlet.http.HttpServletResponse;
             printWriter.println("</head>");
             
             printWriter.println("<body>");
+            printWriter.println("<form action='/pollistServlet'>");
             printWriter.println("<div class='container'>");      
    
 
@@ -63,6 +64,7 @@ import jakarta.servlet.http.HttpServletResponse;
             printWriter.println("src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js' ");
             printWriter.println("integrity='sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4' ");
             printWriter.println("crossorigin='anonymous'></script> ");
+            printWriter.println("</form>");
             printWriter.println("</body> ");
     
             printWriter.close();
