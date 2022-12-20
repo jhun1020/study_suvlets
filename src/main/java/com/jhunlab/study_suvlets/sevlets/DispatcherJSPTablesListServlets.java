0a3636bdd387d00e997fabd8deab567d7843bee6
jhun1020 <jhun1020.kh@gmail.com>
2022-12-20 12:17:41 +0900

    add

---
+++ src/main/java/com/jhunlab/study_suvlets/sevlets/DispatcherJSPTablesListServlets.java
@@ -0,0 +1,31 @@
+package com.jhunlab.study_suvlets.sevlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.jhunlab.beans.MemberBean;
+import com.jhunlab.study_suvlets.DatasInfor;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@WebServlet(urlPatterns = "/dispatcherJSP/TablesListServlets")
+public class DispatcherJSPTablesListServlets extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        DatasInfor datasInfor = new DatasInfor();
+        ArrayList<String> tablesListWithString =   datasInfor.getTablesListWithString();
+        HashMap<String, String> searchForm = datasInfor.getSearchFormData();
+
+        
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/tables_list.jsp");
+        requestDispatcher.forward(request, response);
+
+    }
+}
